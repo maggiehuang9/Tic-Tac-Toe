d0e00207c466f62bd7d588930ec104fa42fd2bb5
Maggie Huang <maggie.huang9@gmail.com>
2020-01-08 18:26:02 -0800

    updated css

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
       <div>{status}</div>
-      <ol>{/* TODO */}</ol>
+      <ol>{}</ol>
         </div>
       </div>
 		);
